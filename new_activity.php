ac06edf7dd96a67e451c864200036bc3746bbb2a
Dingshilun <zgrxdhp@qq.com>
2015-10-04 14:02:47 +0800

    Update new_activity.php

--- new_activity.php
+++ new_activity.php
@@ -1,6 +1,12 @@
 <HTML>
 <BODY>
 <?php
+  session_start();
+  include_once("head.html");
+  include_once( "Menu.php" ); 
+  include_once( "config.php" );
+  include_once( "class.php" );
+  
   $servername="localhost";
   $username="root";
   $password="";
@@ -11,7 +17,7 @@
     $cursor=mysqli_connect($servername,$username,$password,$dbname);
     echo "connection!";
   }
-  $query="insert into activity values('','".$_POST["act_name"]."','1','".$_POST["act_intro"]."','"."remark"."',".$_POST["act_num"].",0,'','')";
+  $query="insert into activity values('','".$_POST["act_name"]."','"-$_SESSION[""]-"','".$_POST["act_intro"]."','"."remark"."',".$_POST["act_num"].",0,'','')";
   if (!mysqli_query($cursor,$query))
   {
    echo mysqli_error($cursor);
@@ -19,8 +25,12 @@
   }
   else
   {
+      $query="select activity_id from activity where activity_name="-$_POST["act_name"]-"and activity_organizer="-$_SESSION[""];
+      mysqli_query($cursor,$query);
+      $_SESSION['act_now']=$cursor;
+      echo "创建活动完毕，转入时间添加界面..."    
   mysqli_close($cursor);
-  $url="/success.html";
+  $url="/time_new.php";
   echo "<script language='javascript' type='text/javascript'>";
   echo "window.location.href='$url'";
   echo "</script>";
